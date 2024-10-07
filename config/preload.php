dfd4e8a424b01d41070db29e098c1340d00f4888
User Friendly <pivanov@user-friendly.dev>
2024-10-07 18:19:07 -0500

    Setup Symfony skeli project.

---
+++ config/preload.php
@@ -0,0 +1,5 @@
+<?php
+
+if (file_exists(dirname(__DIR__).'/var/cache/prod/Orion_KernelProdContainer.preload.php')) {
+    require dirname(__DIR__).'/var/cache/prod/Orion_KernelProdContainer.preload.php';
+}
