b0a7a9de94f9c52242d89b464335992cd0b7f5ad
User Friendly <pivanov@user-friendly.dev>
2024-10-07 17:28:25 -0500

    Add example data.

--- src/index.php
+++ src/index.php
@@ -2,4 +2,14 @@
 
 namespace UserFriendly\Orion;
 
-echo "TODO Implement service";
+$data = [
+  'status' => '500',
+  'message' => 'TODO Implement service',
+  'example' => [1, 2, 3.5, '123', 'Hello!', new \stdClass()]
+];
+
+$output = \json_encode($data, JSON_FORCE_OBJECT);
+
+header('content-type: application/json');
+
+print($output);
